28c24d1bd9e5a1270c253bcef25a9f957be996aa
Jean-Luc Watson <jlwatson@eecs.berkeley.edu>
2024-01-24 15:24:58 -0800

    Protocol draft but errors on 0

--- src/pedersen.rs
+++ src/pedersen.rs
@@ -21,85 +21,18 @@ pub fn setup<T: Rng + CryptoRng>(mut rng: &mut T) -> PublicParams {
     }
 }
 
-pub struct Commitment(RistrettoPoint);
-pub struct Proof(Scalar);
-pub struct Value(Scalar);
-
-impl Value {
-    /*
-    pub fn from_u64(x: u64) -> Self {
-        Value(Scalar::from(x))
-    }
-    */
-
-    pub fn random<T: Rng + CryptoRng>(mut rng: &mut T) -> Self {
-        Value(Scalar::random(&mut rng))
-    }
-}
-
-pub fn commit<T: Rng + CryptoRng>(mut rng: &mut T, val: &Value, params: &PublicParams) -> (Commitment, Proof) {
+pub fn commit<T: Rng + CryptoRng>(mut rng: &mut T, val: &Scalar, params: &PublicParams) -> (RistrettoPoint, Scalar) {
     let r = Scalar::random(&mut rng);
-    let &Value(val_scalar) = val;
 
-    let commitment = RistrettoPoint::multiscalar_mul([val_scalar, r], [params.g, params.h]);
-    (Commitment(commitment), Proof(r))
+    let commitment = RistrettoPoint::multiscalar_mul([val, &r], [params.g, params.h]);
+    (commitment, r)
 }
 
-pub fn commit_with_r(val: &Value, r: &Proof, params: &PublicParams) -> Commitment {
-    let &Value(val_scalar) = val;
-    let &Proof(proof_r) = r;
-
-    let commitment = RistrettoPoint::multiscalar_mul([val_scalar, proof_r], [params.g, params.h]);
-    Commitment(commitment)
+pub fn commit_with_r(val: &Scalar, r: &Scalar, params: &PublicParams) -> RistrettoPoint {
+    RistrettoPoint::multiscalar_mul([val, r], [params.g, params.h])
 }
 
-pub fn verify(commitment: &Commitment, proof: &Proof, val: &Value, params: &PublicParams) -> bool {
-    let &Commitment(commitment_point) = commitment;
-    let &Proof(proof_scalar) = proof;
-    let &Value(val_scalar) = val;
-
-    let lhs = RistrettoPoint::multiscalar_mul([val_scalar, proof_scalar], [params.g, params.h]);
-    lhs == commitment_point
-}
-
-pub fn add_value(val1: &Value, val2: &Value) -> Value {
-    let &Value(val_scalar1) = val1;
-    let &Value(val_scalar2) = val2;
-
-    Value(val_scalar1 + val_scalar2)
-}
-
-pub fn add_comm(commitment1: &Commitment, commitment2: &Commitment) -> Commitment {
-    let &Commitment(commitment_point1) = commitment1;
-    let &Commitment(commitment_point2) = commitment2;
-
-    Commitment(commitment_point1 + commitment_point2)
-}
-
-pub fn add_proof(proof1: &Proof, proof2: &Proof) -> Proof {
-    let &Proof(proof_scalar1) = proof1;
-    let &Proof(proof_scalar2) = proof2;
-
-    Proof(proof_scalar1 + proof_scalar2)
-}
-
-pub fn scale_value(val: &Value, a: &Value) -> Value {
-    let &Value(val_scalar) = val;
-    let &Value(scalar) = a;
-
-    Value(val_scalar * scalar)
-}
-
-pub fn scale_comm(commitment: &Commitment, a: &Value) -> Commitment {
-    let &Commitment(commitment_point) = commitment;
-    let &Value(scalar) = a;
-
-    Commitment(commitment_point * scalar)
-}
-
-pub fn scale_proof(proof: &Proof, a: &Value) -> Proof {
-    let &Proof(proof_scalar) = proof;
-    let &Value(scalar) = a;
-
-    Proof(proof_scalar * scalar)
+pub fn verify(commitment: &RistrettoPoint, proof: &Scalar, val: &Scalar, params: &PublicParams) -> bool {
+    let lhs = RistrettoPoint::multiscalar_mul([val, proof], [params.g, params.h]);
+    lhs == *commitment 
 }
